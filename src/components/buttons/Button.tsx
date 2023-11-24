3ebe6b0eadcd671c44340a9e5b489730ec6746e6
LaupWing <loc_strict@hotmail.com>
2023-11-24 19:35:32 +0100

    button

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,39 @@
+import type { ComponentPropsWithoutRef, FC } from "react"
+import clsx from "clsx"
+
+type ButtonVariant = "default" | "gold"
+
+interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
+   isLoading?: boolean
+   variant?: ButtonVariant
+}
+
+export const Button:FC<ButtonProps> = ({
+   children,
+   className,
+   disabled: buttonDisabled,
+   isLoading,
+   variant = "default",
+   ...props
+}) => {
+   const disabled = isLoading || buttonDisabled
+
+   const variants: Record<ButtonVariant, string> = {
+      default: "bg-light dark:bg-dark text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700",
+      gold: ""
+   }
+
+   return (
+      <button
+         {...props}
+         disabled={disabled}
+         className={clsx(
+            "rounded px-4 py-2 font-bold border border-gray-300 shadow-sm dark:border-gray-600 focus:outline-none focus-visible:ring focus-visible:ring-accent-light scale-100 hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100",
+            variants[variant],
+            className
+         )}
+      >
+         {children}
+      </button>
+   )
+}
