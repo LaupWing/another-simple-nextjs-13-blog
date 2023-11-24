d32f7bbcaf3cd1d68b1f3b4226aa4ee07379a17e
LaupWing <loc_strict@hotmail.com>
2023-11-24 19:46:08 +0100

    insatlled icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,320 @@
+/**
+ * @see https://github.com/vercel/next.js/issues/46756
+ * Migrated to react-icons to make nextjs run smoother
+ */
+import { IconType } from "@react-icons/all-files"
+import { IoNewspaperSharp } from "@react-icons/all-files/io5/IoNewspaperSharp"
+import { GiTechnoHeart } from "@react-icons/all-files/gi/GiTechnoHeart"
+import { IoArrowDownOutline } from "@react-icons/all-files/io5/IoArrowDownOutline"
+import { SiGithub } from "@react-icons/all-files/si/SiGithub"
+import { SiLinkedin } from "@react-icons/all-files/si/SiLinkedin"
+import { SiTwitter } from "@react-icons/all-files/si/SiTwitter"
+import { FiClock } from "@react-icons/all-files/fi/FiClock"
+import { FiEye } from "@react-icons/all-files/fi/FiEye"
+import { FiMail } from "@react-icons/all-files/fi/FiMail"
+import { FiMoon } from "@react-icons/all-files/fi/FiMoon"
+import { FiSun } from "@react-icons/all-files/fi/FiSun"
+import { MdHistory } from "@react-icons/all-files/md/MdHistory"
+import { BiGitRepoForked } from "@react-icons/all-files/bi/BiGitRepoForked"
+import { HiCalendar } from "@react-icons/all-files/hi/HiCalendar"
+import { HiCheck } from "@react-icons/all-files/hi/HiCheck"
+import { HiCheckCircle } from "@react-icons/all-files/hi/HiCheckCircle"
+import { HiClipboard } from "@react-icons/all-files/hi/HiClipboard"
+import { HiLink } from "@react-icons/all-files/hi/HiLink"
+import { HiOutlineStar } from "@react-icons/all-files/hi/HiOutlineStar"
+import { HiSortAscending } from "@react-icons/all-files/hi/HiSortAscending"
+import { HiSelector } from "@react-icons/all-files/hi/HiSelector"
+import { SiFirebase } from "@react-icons/all-files/si/SiFirebase"
+import { SiGit } from "@react-icons/all-files/si/SiGit"
+import { SiGatsby } from "@react-icons/all-files/si/SiGatsby"
+import { SiJavascript } from "@react-icons/all-files/si/SiJavascript"
+import { SiMarkdown } from "@react-icons/all-files/si/SiMarkdown"
+import { SiMongodb } from "@react-icons/all-files/si/SiMongodb"
+import { SiNextDotJs } from "@react-icons/all-files/si/SiNextDotJs"
+import { SiNodeDotJs } from "@react-icons/all-files/si/SiNodeDotJs"
+import { SiReact } from "@react-icons/all-files/si/SiReact"
+import { SiRedux } from "@react-icons/all-files/si/SiRedux"
+import { SiTailwindcss } from "@react-icons/all-files/si/SiTailwindcss"
+import { SiTypescript } from "@react-icons/all-files/si/SiTypescript"
+import { SiWordpress } from "@react-icons/all-files/si/SiWordpress"
+import { IoLogoVercel } from "@react-icons/all-files/io5/IoLogoVercel"
+
+export const IconNewspaper:IconType = (props) => {
+   return (
+      <IoNewspaperSharp 
+         {...props}
+      />
+   )
+}
+
+export const IconSortAscending:IconType = (props) => {
+   return (
+      <HiSortAscending 
+         {...props}
+      />
+   )
+}
+
+export const IconVercel:IconType = (props) => {
+   return (
+      <IoLogoVercel
+         {...props}
+      />
+   )
+}
+
+export const IconWordpress:IconType = (props) => {
+   return (
+      <SiWordpress
+         {...props}
+      />
+   )
+}
+
+export const IconTypescript:IconType = (props) => {
+   return (
+      <SiTypescript
+         {...props}
+      />
+   )
+}
+
+export const IconTailwindcss:IconType = (props) => {
+   return (
+      <SiTailwindcss
+         {...props}
+      />
+   )
+}
+
+export const IconRedux:IconType = (props) => {
+   return (
+      <SiRedux
+         {...props}
+      />
+   )
+}
+
+export const IconReact:IconType = (props) => {
+   return (
+      <SiReact
+         {...props}
+      />
+   )
+}
+
+export const IconNodeDotJs:IconType = (props) => {
+   return (
+      <SiNodeDotJs
+         {...props}
+      />
+   )
+}
+
+export const IconNextDotJs:IconType = (props) => {
+   return (
+      <SiNextDotJs 
+         {...props}
+      />
+   )
+}
+
+export const IconMongodb:IconType = (props) => {
+   return (
+      <SiMongodb 
+         {...props}
+      />
+   )
+}
+
+export const IconMarkdown:IconType = (props) => {
+   return (
+      <SiMarkdown 
+         {...props}
+      />
+   )
+}
+
+export const IconJavascript:IconType = (props) => {
+   return (
+      <SiJavascript
+         {...props}
+      />
+   )
+}
+
+export const IconGatsby:IconType = (props) => {
+   return (
+      <SiGatsby 
+         {...props}
+      />
+   )
+}
+
+export const IconGit:IconType = (props) => {
+   return (
+      <SiGit 
+         {...props}
+      />
+   )
+}
+
+export const IconFirebase:IconType = (props) => {
+   return (
+      <SiFirebase
+         {...props}
+      />
+   )
+}
+
+export const IconHeart:IconType = (props) => {
+   return (
+      <GiTechnoHeart 
+         {...props}
+      />
+   )
+}
+
+export const IconArrowDown:IconType = (props) => {
+   return (
+      <IoArrowDownOutline
+         {...props}
+      />
+   )
+}
+
+export const IconGithub:IconType = (props) => {
+   return (
+      <SiGithub
+         {...props}
+      />
+   )
+}
+
+export const IconTwitter:IconType = (props) => {
+   return (
+      <SiTwitter
+         {...props}
+      />
+   )
+}
+
+export const IconSun:IconType = (props) => {
+   return (
+      <FiSun
+         {...props}
+      />
+   )
+}
+
+export const IconMoon:IconType = (props) => {
+   return (
+      <FiMoon
+         {...props}
+      />
+   )
+}
+
+export const IconClock:IconType = (props) => {
+   return (
+      <FiClock
+         {...props}
+      />
+   )
+}
+
+export const IconEye:IconType = (props) => {
+   return (
+      <FiEye
+         {...props}
+      />
+   )
+}
+
+export const IconLinkedin:IconType = (props) => {
+   return (
+      <SiLinkedin
+         {...props}
+      />
+   )
+}
+
+export const IconMail:IconType = (props) => {
+   return (
+      <FiMail
+         {...props}
+      />
+   )
+}
+
+export const IconCalendar:IconType = (props) => {
+   return (
+      <HiCalendar
+         {...props}
+      />
+   )
+}
+
+export const IconSelector:IconType = (props) => {
+   return (
+      <HiSelector
+         {...props}
+      />
+   )
+}
+
+export const IconCheckmark:IconType = (props) => {
+   return (
+      <HiCheck
+         {...props}
+      />
+   )
+}
+
+export const IconHistory:IconType = (props) => {
+   return (
+      <MdHistory
+         {...props}
+      />
+   )
+}
+
+export const IconClipboard:IconType = (props) => {
+   return (
+      <HiClipboard
+         {...props}
+      />
+   )
+}
+
+export const IconCheckCircle:IconType = (props) => {
+   return (
+      <HiCheckCircle
+         {...props}
+      />
+   )
+}
+
+export const IconStar:IconType = (props) => {
+   return (
+      <HiOutlineStar
+         {...props}
+      />
+   )
+}
+
+export const IconRepoForked:IconType = (props) => {
+   return (
+      <BiGitRepoForked
+         {...props}
+      />
+   )
+}
+
+export const IconLink:IconType = (props) => {
+   return (
+      <HiLink
+         {...props}
+      />
+   )
+}
