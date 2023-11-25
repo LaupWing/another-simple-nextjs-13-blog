26634debd0d747ce83501d3a7ce6919854c76ae0
LaupWing <loc_strict@hotmail.com>
2023-11-25 17:33:29 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Accent } from "@/components/elements/Accent"
 import Image from 'next/image'
 
 export default function Home() {
@@ -17,6 +18,9 @@ const HomeIntro = () => {
             <h2 className="text-2xl md:text-4xl 2xl:text-5xl">
                Hello!
             </h2>
+            <h1 className="mt-1 text-3xl md:text-5xl 2xl:text-6xl">
+               My name is <Accent>Laup Wing </Accent>
+            </h1>
          </div>
       </section>
    )
