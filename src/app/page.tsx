8aef2ba600a939a65e82080dc64fce2c4844df41
LaupWing <loc_strict@hotmail.com>
2023-11-25 18:02:28 +0100

    added screen height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 const HomeIntro = () => {
 
    return (
-      <section className="flex flex-col items-center justify-center min-h-screen">
+      <section className="flex flex-col items-center justify-center screen-height-minus-nav">
          <div className="custom-container">
             <h2 className="text-2xl font-bold md:text-4xl 2xl:text-5xl">
                Hello!
