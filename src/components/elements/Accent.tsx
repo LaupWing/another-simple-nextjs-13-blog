9d12d4a50f8517e913dff6680093d0add6f4af84
LaupWing <loc_strict@hotmail.com>
2023-11-25 17:38:32 +0100

    update

--- src/components/elements/Accent.tsx
+++ src/components/elements/Accent.tsx
@@ -11,7 +11,7 @@ export const Accent:FC<ComponentPropsWithoutRef<"span">> = ({
          <span
             className={clsx(
                className,
-               "transition-colors from-accent-light bg-gradient-to-r to-accent-dark bg-clip-text text-transparent dark:text-dark"
+               "transition-colors gradient-animation-slow bg-clip-text text-transparent dark:text-dark"
             )}
          >
             {children}
