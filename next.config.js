f48b89691599256811d37112da33d0e557ed79be
LaupWing <loc_strict@hotmail.com>
2023-11-28 19:28:49 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      domains: [
-         "res.cloudinary.com"
-      ]
-   }
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "**.cloudinary.com"
+         },
+      ],
+   },
 }
 
 module.exports = nextConfig
