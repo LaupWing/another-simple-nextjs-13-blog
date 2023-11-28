a738d355e6d5f272a86fa77588ce19ad9f70241e
LaupWing <loc_strict@hotmail.com>
2023-11-28 17:53:44 +0100

    Card container

---
+++ src/components/containers/CardContainer.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { BlogCard } from "@/components/cards/BlogCard.client"
+import useInjectContentMeta from "@/hooks/useInjectContentMeta"
+import { BlogFrontmatter } from "@/types/frontmatters"
+import { FC, JSX } from "react"
+
+interface BlogCardsContainerProps {
+   posts: BlogFrontmatter[],
+   Cmp: Component
+}
+
+const BlogCardsContainer:FC<BlogCardsContainerProps> = ({
+   posts,
+   Cmp
+}) => {
+   const populatedBlogs = useInjectContentMeta(posts)
+
+   return (
+      <>
+         {populatedBlogs.map(post => (
+            <Cmp
+               key={post.slug}
+               post={post as BlogFrontmatter}
+            />
+         ))}
+      </>
+   )
+}
+export default BlogCardsContainer
