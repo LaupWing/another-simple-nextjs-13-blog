2b9bd1f7b4b7dd7fdb033a5aeb07458d1bbff59f
LaupWing <loc_strict@hotmail.com>
2023-11-29 18:29:08 +0100

    body dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <html 
          lang="en"
       >
-         <body className={inter.className}>
+         <body className={`${inter.className} dark:bg-dark`}>
             <Header />
             <NextTopLoader />
             {children}
