e1e1e18d7ff8cb2a219789aa0a38850b02be95af
LaupWing <loc_strict@hotmail.com>
2023-11-29 18:30:21 +0100

    dark butto

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -20,12 +20,12 @@ export const Button:FC<ButtonProps> = ({
 
    
    const variants: Record<ButtonVariant, string> = {
-      default: "bg-light dark:bg-dark text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700 py-2 border border-gray-300 dark:border-gray-600 focus:outline-none focus-visible:ring focus-visible:ring-accent shadow-sm hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100",
+      default: "py-2 border border-gray-300 dark:border-gray-600 focus:outline-none focus-visible:ring focus-visible:ring-accent shadow-sm hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100",
       "gradient-animation": "bg-white py-[7px]"
    }
 
    const classNameComputed = clsx(
-      "rounded font-bold px-4 scale-100",
+      "rounded font-bold px-4 scale-100 bg-light dark:bg-dark text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700 ",
       variants[variant],
       className
    )
