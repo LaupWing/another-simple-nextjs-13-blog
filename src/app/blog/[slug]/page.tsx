76fccb2395e79da08bf41da73e35cc0cd31dcf94
LaupWing <loc_strict@hotmail.com>
2023-11-30 14:12:00 +0100

    Like button loading

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -80,17 +80,7 @@ const SingleBlogPage = async (props: PageProps) => {
                      <Suspense>
 
                      </Suspense>
-                     <div className="animate-pulse flex items-center space-x-4">
-                        <svg
-                           viewBox="0 0 231.18 354.53"
-                           className="text-gray-500 fill-current w-14"
-                        >
-                           <path d={CHAD_PATH} />
-                        </svg>
-                        <div className="text-gray-400 dark:text-gray-500 mt-1 text-lg font-medium">
-                           <span>...</span>
-                        </div>
-                     </div>
+                     
                      {/* <LikeButton /> */}
                   </div>
                </div>
