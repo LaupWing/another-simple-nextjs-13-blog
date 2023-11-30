79a9589785565c39a39609c3d93bd5407fc20a63
LaupWing <loc_strict@hotmail.com>
2023-11-30 10:29:54 +0100

    cnnoot do circular imports

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,28 +1,28 @@
 import { CustomLink } from "./links"
 import Image from "next/image"
 import { CloudinaryImage } from "./images/index.client"
-// import { CustomCode } from "./sections/index.client"
-// import LiteYouTubeEmbed from "react-lite-youtube-embed"
-// import { 
-//    Pre, 
-//    Split, 
-//    SplitImage 
-// } from "./sections"
-// import { TechIcons } from "./TechIcons.client"
-// import { TweetCard } from "./cards"
-// import { GithubCard } from "./cards/index.client"
+import { CustomCode } from "./sections/CustomCode.client"
+import LiteYouTubeEmbed from "react-lite-youtube-embed"
+import { 
+   Pre, 
+   Split, 
+   SplitImage 
+} from "./sections"
+import { TechIcons } from "./TechIcons.client"
+import { TweetCard } from "./cards"
+import { GithubCard } from "./cards/index.client"
 
 
 export const MDXComponents = {
    a: CustomLink,
    Image,
    Cloudinary: CloudinaryImage,
-   // code: CustomCode,
-   // pre: Pre,
-   // LiteYouTubeEmbed,
-   // Split,
-   // SplitImage,
-   // TechIcons,
-   // TweetCard,
-   // GithubCard
+   code: CustomCode,
+   pre: Pre,
+   LiteYouTubeEmbed,
+   Split,
+   SplitImage,
+   TechIcons,
+   TweetCard,
+   GithubCard
 }
