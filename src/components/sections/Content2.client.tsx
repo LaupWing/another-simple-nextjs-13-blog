1f2b744cfb4502b18a7400eb8c2caa01916f679a
LaupWing <loc_strict@hotmail.com>
2023-11-30 10:17:34 +0100

    removed more unused code

--- src/components/sections/Content2.client.tsx
+++ src/components/sections/Content2.client.tsx
@@ -2,16 +2,13 @@
 import type { FC } from "react"
 
 import { useMemo } from "react"
-import { BlogFrontmatter } from "@/types/frontmatters"
 import { getMDXComponent } from "mdx-bundler/client"
 
 interface ContentProps {
-   frontmatter: BlogFrontmatter
    code: string
 }
 export const Content2:FC<ContentProps> = ({
-   code,
-   frontmatter
+   code
 }) => {
    const Component = useMemo(() => getMDXComponent(code), [code])
    // console.log(code)
