a6aa486a95f29ae766412f6473c6e6a89e594891
LaupWing <loc_strict@hotmail.com>
2023-11-30 10:21:50 +0100

    removed content import for now

--- src/components/sections/index.client.ts
+++ src/components/sections/index.client.ts
@@ -1,3 +1,3 @@
 export { CustomCode } from "./CustomCode.client"
-export { Content } from "./Content.client"
+// export { Content } from "./Content.client"
 export { Content2 } from "./Content2.client"
