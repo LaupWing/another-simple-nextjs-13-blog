f4670f739a04a5194462fd48cf92a164cc7f9e52
LaupWing <loc_strict@hotmail.com>
2023-11-30 10:07:05 +0100

    noice

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,15 @@
+import { IconClock, IconEye, IconHistory } from "@/components/Icons"
+import { Accent } from "@/components/elements"
+import { CloudinaryImage } from "@/components/images/CloudinaryImage.client"
+import { UnstyledLink } from "@/components/links/UnstyledLink.client"
 import { Content2 } from "@/components/sections/index.client"
 // import { Content } from "@/components/sections/index.client"
 import { getFileBySlug, getFiles } from "@/lib/mdx"
 import { BlogFrontmatter } from "@/types/frontmatters"
+import { format } from "date-fns"
 // import ContentSection from "../../components/blog/ContentSection.client"
 import { Metadata } from "next"
+import { FC } from "react"
 // import seo from "@/lib/seo"
 
 export const dynamicParams = false
@@ -55,13 +61,80 @@ const SingleBlogPage = async (props: PageProps) => {
    } = post
 
    return (
-      <main>
-         <Content2
-            code={code}
+      <main className="custom-container">
+         <Header
             frontmatter={frontmatter}
          />
+         {/* <Content2
+            code={code}
+            frontmatter={frontmatter}
+         /> */}
       </main>
    )
 }
 
 export default SingleBlogPage
+
+interface HeaderProps {
+   frontmatter: BlogFrontmatter
+}
+
+const Header:FC<HeaderProps> = ({
+   frontmatter
+}) => {
+   const COMMIT_HISTORY_LINK = `https://github.com/LaupWing/tech-blog/commits/main/src/contents/blog/${frontmatter.slug}.mdx`
+   // const meta = useContentMeta(frontmatter.slug, {
+   //    runIncrement: true
+   // })
+   
+   return (
+      <header className="pb-4">
+         <CloudinaryImage
+            public_id="samples/bike"
+            alt="Bike"
+            width={1200}
+            height={(1200 * 2) /5}
+            aspect={{
+               height: 2,
+               width: 5
+            }}
+         />
+         <h1 className="mt-4">{frontmatter.title}</h1>
+         <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
+            Writton on{" "}
+            {format(new Date(frontmatter.publishedAt), "MMMM dd, yyyy")} by
+            Laup Wing
+         </p>
+         {frontmatter.lastUpdated && (
+            <div className="mt-2 flex flex-wrap gap-2 text-sm text-gray-700 dark:text-gray-200">
+               <p>
+                  Last updated{" "}
+                  {format(new Date(frontmatter.lastUpdated), "MMMM dd, yyyy")}
+                  .
+               </p>
+               <UnstyledLink
+                  href={COMMIT_HISTORY_LINK}
+                  className="inline-flex items-center gap-1 rounded-sm font-medium text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-accent-light focus:outline-none focus-visible:ring focus-visible:ring-accent-dark"
+               >
+                  <IconHistory className="text-lg" />
+                  <span>See changes</span>
+               </UnstyledLink>
+            </div>
+         )}
+         <div className="mt-6 flex items-center justify-start gap-2 text-sm font-medium text-gray-600 dark:text-gray-300">
+            <div className="flex items-center gap-1">
+               <IconClock className="inline-block text-base" />
+               <Accent>{frontmatter.readingTime.text}</Accent>
+            </div>
+            <div className="flex items-center gap-1">
+               <IconEye className="text-base inline-block" />
+               {/* {meta.isLoading ? (
+                  <Accent className="animate-pulse"> --- views</Accent>
+               ) :( 
+                  <Accent>{meta.views} views</Accent>
+               )} */}
+            </div>
+         </div>
+      </header>
+   )
+}
