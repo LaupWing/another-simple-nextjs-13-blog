92916d5f7f91f3c9745e96072fcab50641957325
LaupWing <loc_strict@hotmail.com>
2023-11-30 09:05:32 +0100

    index client

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image"
 import { CustomLink } from "./links"
 import { CloudinaryImage } from "./images/index.client"
+import { CustomCode } from "./sections/index.client"
 
 
 export const MDXComponents = {
    a: CustomLink,
    Image,
    Cloudinary: CloudinaryImage,
+   code: CustomCode
 }
---
+++ src/components/sections/index.client.ts
@@ -0,0 +1,1 @@
+export { CustomCode } from "./CustomCode"
