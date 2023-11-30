6dbf03e47bc4b3ecddc178fef3f3988cd14d8e7c
LaupWing <loc_strict@hotmail.com>
2023-11-30 14:24:59 +0100

    get all likes and likes

--- src/app/api/like/[slug]/route.ts
+++ src/app/api/like/[slug]/route.ts
@@ -2,6 +2,47 @@ import { extractSlug, getSessionId, getUserLikeCount } from "@/lib/helper.server
 import { prisma_client } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
+export async function GET(req: Request) {
+   try{
+      const slug = extractSlug(req)
+      const session_id = getSessionId(req)
+      const likeCount = await getUserLikeCount({ session_id, slug })
+      
+      const content = await prisma_client.contentMeta.findFirst({
+         where: {
+            slug: slug
+         },
+         include: {
+            _count: {
+               select: {
+                  Like: true
+               }
+            }
+         }
+      })
+
+      return NextResponse.json({
+         all_likes: content?._count.Like ?? 0
+      }, {
+         status: 200
+      })
+   } catch(e: unknown){
+      if (e instanceof Error) {
+         return NextResponse.json({
+            message: e.message ?? "Internal Server Error"
+         }, {
+            status: 500
+         })
+      } else {
+         return NextResponse.json({
+            message: "Internal Server Error"
+         }, {
+            status: 500
+         })
+      }
+   }
+}
+
 export async function POST(req: Request) {
    try {
       const session_id = getSessionId(req)
