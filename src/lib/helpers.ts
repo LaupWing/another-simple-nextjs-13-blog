2f5e532e147c512b62427a2497807a2c7fd3636b
LaupWing <loc_strict@hotmail.com>
2023-12-01 09:56:22 +0100

    restyled the naming

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,38 +1,38 @@
 import type { ContentType, PickFrontmatter } from "@/types/frontmatters"
 
 interface OpenGraphType {
-   siteName: string
+   site_name: string
    description: string
-   templateTitle?: string
+   template_title?: string
    logo?: string
    banner?: string
-   isBlog?: boolean
+   is_blog?: boolean
 }
 
 export const openGraph = ({
-   siteName,
-   templateTitle,
+   site_name,
+   template_title,
    description,
    banner,
    logo = `${process.env.SITE_URL}/images/logo.png`,
-   isBlog = false
+   is_blog = false
 }: OpenGraphType) : string => {
-   const ogLogo = encodeURIComponent(logo)
-   const ogSiteName = encodeURIComponent(siteName.trim())
-   const ogTemplateTitle = templateTitle
-      ? encodeURIComponent(templateTitle.trim())
+   const og_logo = encodeURIComponent(logo)
+   const og_sitename = encodeURIComponent(site_name.trim())
+   const og_template_title = template_title
+      ? encodeURIComponent(template_title.trim())
       : undefined
-   const ogDescription = encodeURIComponent(description.trim())
+   const og_description = encodeURIComponent(description.trim())
 
-   if(isBlog) {
-      const ogBanner = banner 
+   if(is_blog) {
+      const og_banner = banner 
          ? encodeURIComponent(banner.trim())
          : undefined
       
-      return `${process.env.SITE_URL}/api/og/blog?templateTitle=${ogTemplateTitle}&banner=${ogBanner}`
+      return `${process.env.SITE_URL}/api/og/blog?templateTitle=${og_template_title}&banner=${og_banner}`
    }
 
-   return `${process.env.SITE_URL}/api/og/gradient?siteName=${ogSiteName}&description=${ogDescription}&logo=${ogLogo}${ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ""}`
+   return `${process.env.SITE_URL}/api/og/gradient?siteName=${og_sitename}&description=${og_description}&logo=${og_logo}${og_template_title ? `&templateTitle=${og_template_title}` : ""}`
 }
 
 export const getFromSessionStorage = (key: string) => {
