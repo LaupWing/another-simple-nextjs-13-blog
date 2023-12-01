0bb4dc93754ec11efe1aaacdf9cc69403e17f2bf
LaupWing <loc_strict@hotmail.com>
2023-12-01 10:05:46 +0100

    seperated hero

--- src/app/library/[slug]/page.tsx
+++ src/app/library/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getFileBySlug, getFiles } from "@/lib/mdx"
 import { LibraryFrontmatter } from "@/types/frontmatters"
+import { FC } from "react"
 // import ContentSection from "../../components/library/ContentSection.client"
 // import { Metadata } from "next"
 // import seo from "@/lib/seo"
@@ -47,12 +48,38 @@ const SingleLibraryPage = async (props: PageProps) => {
       code
    } = post
    return (
-      <section className="layout">
-         <ContentSection
-            code={code}
-            frontmatter={frontmatter}
-         />
-      </section>
+      <main className="custom-container">
+         
+      </main>
    )
 }
 export default SingleLibraryPage
+
+interface HeroProps {
+   frontmatter: LibraryFrontmatter
+}
+
+const Hero:FC<HeroProps> = ({ frontmatter }) => {
+   return (
+      <div className="border-b pb-4 dark:border-gray-600">
+         <h1>{frontmatter.title}</h1>
+         <p className="mt-2 text-sm border-gray-600 dark:text-gray-300">
+            {frontmatter.description}
+         </p>
+         <div className="mt-2 flex items-center justify-start gap-3 text-sm font-medium text-gray-600 dark:text-gray-300">
+            <div className="flex items-center gap-1">
+               <IconEye className="inline-block text-base" />
+               {meta.isLoading ? (
+                  <Accent className="animate-pulse"> --- views</Accent>
+               ) :( 
+                  <Accent>{meta.views} views</Accent>
+               )}
+            </div>
+            <span>•</span>
+            <TechIcons 
+               techs={frontmatter.tags.split(",") as Array<TechListType>}
+            />
+         </div>
+      </div>
+   )
+}
