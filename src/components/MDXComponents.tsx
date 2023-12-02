19ccef36c7fce9c0208fa7b352125ac20c4f2e2f
LaupWing <loc_strict@hotmail.com>
2023-12-02 15:28:11 +0100

    removed

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -3,11 +3,9 @@ import Image from "next/image"
 import { CloudinaryImage } from "./images/CloudinaryImage.client"
 import { CustomCode } from "./sections/CustomCode.client"
 import LiteYouTubeEmbed from "react-lite-youtube-embed"
-import { 
-   Pre, 
-   Split, 
-   SplitImage 
-} from "./sections"
+import { Pre } from "./sections/Pre"
+import { SplitImage } from "./sections/SplitImage"
+import { Split,  } from "./sections/Split"
 import { TechIcons } from "./TechIcons.client"
 import { TweetCard } from "./cards/TweetCard"
 import { GithubCard } from "./cards/GithubCard.client"
--- src/components/sections/index.ts
+++
@@ -1,3 +0,0 @@
-export { Pre } from "./Pre"
-export { SplitImage } from "./SplitImage"
-export { Split } from "./Split"
