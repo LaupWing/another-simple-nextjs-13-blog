71467b5c4d9efcad7c6ef6d447900eb78890a02e
LaupWing <loc_strict@hotmail.com>
2023-12-02 16:43:35 +0100

    udpated

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const default_meta = {
-   title: "Laup Wing Tech",
-   siteName: "tech.laupwing.com",
-   description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet necessitatibus doloribus, omnis delectus obcaecati reprehenderit inventore esse at cum tenetur? Sapiente tenetur molestias odio labore quaerat omnis eos officiis corporis.",
+   title: "Software Developer - Loc Nguyen",
+   siteName: "loc-nguyen.com",
+   description: "Tech brilliance meets freelance versatility! I'm Loc Nguyen, a full-stack web developer and tech blogger. Unleash the power of code through my insights, and let's turn your ideas into digital masterpieces. Open for freelance gigs – let's build something extraordinary! 💻✨",
    url: process.env.SITE_URL,
    // Need to replace this
    // og_image: "https://www.cgsusa.org/wp-content/uploads/cropped-placeholder.jpg",
