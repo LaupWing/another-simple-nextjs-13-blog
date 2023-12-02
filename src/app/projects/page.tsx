25d946a229e5c728ffdb16bcc81e83e7a447082d
LaupWing <loc_strict@hotmail.com>
2023-12-02 14:45:02 +0100

    Loading ocntianer

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import { ProjectCard } from "@/components/cards/ProjectCard"
 import { Accent } from "@/components/elements/Accent"
 import { getAllFilesFrontmatter } from "@/lib/mdx"
+import { LoadedContainer } from "@/components/containers/LoadedContainer.client"
 
 const fetchProjects = async () => {
    const projects = await getAllFilesFrontmatter("projects")
@@ -12,22 +13,33 @@ const Projects = async () => {
    const projects = await fetchProjects()
 
    return (
-      <section className="custom-container py-12">
-         <h1 className="text-3xl md:text-5xl">
-            <Accent>Projects</Accent>
-         </h1>
-         <p className="mt-2 text-gray-600 dark:text-gray-300">
-            My favorite projects.
-         </p>
-         <ul className="mt-6 grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
-            {projects.map((project) => (
-               <ProjectCard 
-                  key={project.slug}
-                  project={project}
-               />
-            ))}
-         </ul>
-      </section>
+      <LoadedContainer>
+         <section className="custom-container py-12">
+            <h1 
+               className="text-3xl md:text-5xl"
+               data-fade="0"
+            >
+               <Accent>Projects</Accent>
+            </h1>
+            <p 
+               className="mt-2 text-gray-600 dark:text-gray-300"
+               data-fade="1"
+            >
+               My favorite projects.
+            </p>
+            <ul 
+               className="mt-6 grid gap-4 sm:grid-cols-2 xl:grid-cols-3"
+               data-fade="2"
+            >
+               {projects.map((project) => (
+                  <ProjectCard 
+                     key={project.slug}
+                     project={project}
+                  />
+               ))}
+            </ul>
+         </section>
+      </LoadedContainer>
    )
 }
 export default Projects
