9e1b92fe92b025008717a2c60364cbd7bcadf289
LaupWing <loc_strict@hotmail.com>
2023-12-03 16:48:24 +0100

    added scale 1

--- src/components/elements/Tag.tsx
+++ src/components/elements/Tag.tsx
@@ -16,7 +16,7 @@ export const Tag:FC<TagProps> = ({
          <button
             className={clsx(
                className,
-               "inline-block rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:bg-gray-200 disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 dark:disabled:text-gray-500 focus:outline-none focus-visible:ring-accent-light focus-visible:ring disabled:cursor-not-allowed"
+               "inline-block scale-100 rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:bg-gray-200 disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 dark:disabled:text-gray-500 focus:outline-none focus-visible:ring-accent-light focus-visible:ring disabled:cursor-not-allowed"
             )}
             {...props}
          >
