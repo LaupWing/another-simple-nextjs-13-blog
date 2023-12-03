dec02087cbba60f9ae7d8861704b8790463646f7
LaupWing <loc_strict@hotmail.com>
2023-12-03 09:20:34 +0100

    gradient border types

--- src/components/elements/GradientBorder.tsx
+++ src/components/elements/GradientBorder.tsx
@@ -1,9 +1,12 @@
+import type { FC, PropsWithChildren } from "react"
 
 
-export const GradientBorder = () => {
+export const GradientBorder:FC<PropsWithChildren> = ({
+   children
+}) => {
    return (
       <div className="gradient-animation-border shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100">
-         
+         { children }
       </div>
    )
 }
