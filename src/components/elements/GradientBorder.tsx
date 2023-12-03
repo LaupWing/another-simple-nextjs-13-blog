59c8502beda38006331f66d83c9a83cd1620c686
LaupWing <loc_strict@hotmail.com>
2023-12-03 09:20:05 +0100

    copied gradient border

--- src/components/elements/GradientBorder.tsx
+++ src/components/elements/GradientBorder.tsx
@@ -2,6 +2,8 @@
 
 export const GradientBorder = () => {
    return (
-      <div>Gradient</div>
+      <div className="gradient-animation-border shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100">
+         
+      </div>
    )
 }
