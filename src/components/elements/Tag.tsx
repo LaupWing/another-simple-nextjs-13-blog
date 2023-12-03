f9224037b10cc6b16907ae8407888cb33ade1c58
LaupWing <loc_strict@hotmail.com>
2023-12-03 16:56:00 +0100

    added onlickc

--- src/components/elements/Tag.tsx
+++ src/components/elements/Tag.tsx
@@ -4,24 +4,36 @@ import { GradientBorder } from "./GradientBorder"
 
 interface TagProps extends ComponentPropsWithoutRef<"button"> {
    active: boolean
+   onClick?: () => void
 }
 
 export const Tag:FC<TagProps> = ({
    children,
    className,
-   ...props
+   onClick,
+   active
 }) => {
-   return (
+   return active ? (
       <GradientBorder className="!p-0.5">
          <button
+            onClick={onClick}
             className={clsx(
                className,
                "inline-block scale-100 rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:bg-gray-200 disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 dark:disabled:text-gray-500 focus:outline-none focus-visible:ring-accent-light focus-visible:ring disabled:cursor-not-allowed"
             )}
-            {...props}
          >
             {children}
          </button>
       </GradientBorder>
+   ) : (
+      <button
+         onClick={onClick}
+         className={clsx(
+            className,
+            "inline-block scale-100 rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:bg-gray-200 disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 dark:disabled:text-gray-500 focus:outline-none focus-visible:ring-accent-light focus-visible:ring disabled:cursor-not-allowed"
+         )}
+      >
+         {children}
+      </button>
    )
 }
