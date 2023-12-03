4ee03dcdaa66e1aebe0c5b26214d7f5592d55fb8
LaupWing <loc_strict@hotmail.com>
2023-12-03 16:45:06 +0100

    gradient border

--- src/components/elements/Tag.tsx
+++ src/components/elements/Tag.tsx
@@ -1,5 +1,6 @@
 import type { ComponentPropsWithoutRef, FC } from "react"
 import clsx from "clsx"
+import { GradientBorder } from "./GradientBorder"
 
 interface TagProps extends ComponentPropsWithoutRef<"button"> {
    active: boolean
@@ -11,14 +12,16 @@ export const Tag:FC<TagProps> = ({
    ...props
 }) => {
    return (
-      <button
-         className={clsx(
-            className,
-            "inline-block rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:bg-gray-200 disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 dark:disabled:text-gray-500 focus:outline-none focus-visible:ring-accent-light focus-visible:ring disabled:cursor-not-allowed"
-         )}
-         {...props}
-      >
-         {children}
-      </button>
+      <GradientBorder>
+         <button
+            className={clsx(
+               className,
+               "inline-block rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:bg-gray-200 disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 dark:disabled:text-gray-500 focus:outline-none focus-visible:ring-accent-light focus-visible:ring disabled:cursor-not-allowed"
+            )}
+            {...props}
+         >
+            {children}
+         </button>
+      </GradientBorder>
    )
 }
