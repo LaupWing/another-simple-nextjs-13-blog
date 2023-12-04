b9b7b98d5594a462b90f26f5ede3867205290053
LaupWing <loc_strict@hotmail.com>
2023-12-04 10:21:22 +0100

    added

--- src/components/sections/ContactMe.client.tsx
+++ src/components/sections/ContactMe.client.tsx
@@ -8,25 +8,29 @@ export const ContactMe = () => {
    return (
       <form
          ref={form_ref}
-         className="flex custom-container items-end gap-2"
+         className="flex flex-col custom-container"
       >
-         <div className="flex flex-col w-full max-w-[300px]">
-            <label 
-               htmlFor="email"
-               className="uppercase tracking-wider text-sm font-bold text-gray-600 dark:text-gray-400"
-            >
-               Email
-            </label>
-            <input 
-               className="py-2 px-2 w-full rounded text-black bg-gray-100 border-2 border-gray-300"
-               type="email" 
-               name="email" 
-               id="email" 
-            />
+         <p>Subscribe to my newsletter to get regular content not published on my site.</p>
+         <div className="flex items-end gap-2">
+            <div className="flex gap-1 flex-col w-full max-w-[300px]">
+               <label 
+                  htmlFor="email"
+                  className="uppercase tracking-wider text-sm font-bold text-gray-600 dark:text-gray-400"
+               >
+                  Email
+               </label>
+               <input 
+                  className="py-2 px-2 w-full rounded text-black bg-gray-100 border-2 border-gray-300"
+                  type="email" 
+                  name="email" 
+                  id="email" 
+                  placeholder="Enter your email"
+               />
+            </div>
+            <Button variant="gradient-animation">
+               Submit
+            </Button>
          </div>
-         <Button variant="gradient-animation">
-            Submit
-         </Button>
       </form>
    )
 }
