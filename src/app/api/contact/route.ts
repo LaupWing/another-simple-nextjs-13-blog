75e5fc6388a0b7fd59593fa4144385d80af8ce1c
LaupWing <loc_strict@hotmail.com>
2023-12-04 12:15:16 +0100

    wrapped in try catc

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -4,31 +4,35 @@ import nodemailer from "nodemailer"
 export async function POST(request: Request) {
    const data = await request.json()
    const { name, email, message } = data
-
-   const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-         user: process.env.GMAIL_USER,
-         pass: process.env.GMAIL_APP_PASSWORD
-      }
-   })
-
-   await transporter.sendMail({
-      from: email,
-      to: process.env.GMAIL_USER,
-      subject: `New message from ${name}`,
-      text: message
-   })
-
-   await prisma_client.contact.create({
-      data: {
-         name,
-         email,
-         message
-      }
-   })
+   try {
+      const transporter = nodemailer.createTransport({
+         host: "smtp.gmail.com",
+         port: 465,
+         secure: true,
+         auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_APP_PASSWORD
+         }
+      })
    
-   return Response.json({ message: "ok" })
+      await transporter.sendMail({
+         from: email,
+         to: process.env.GMAIL_USER,
+         subject: `New message from ${name}`,
+         text: message
+      })
+   
+      await prisma_client.contact.create({
+         data: {
+            name,
+            email,
+            message
+         }
+      })
+      
+      return Response.json({ message: "ok" })
+   } catch(err) {
+      console.log(err)
+   }
+
 }
