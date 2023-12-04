c540d9d06fc7c056dc7c8894536614fe3a0ec76e
LaupWing <loc_strict@hotmail.com>
2023-12-04 10:57:35 +0100

    udpate

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,17 @@ const Contact = () => {
          >
             <Accent>Contact</Accent>
          </h1>
-         
+         <p>Unlock exciting possibilities! Interested in a project or tutoring? Reach out using the contact form and let's embark on this journey together!</p>
+         <form className="mt-4 flex w-full max-w-lg flex-col">
+            <input 
+               type="email" 
+               className="rounded-md text-black px-2 py-2 border-2 border-gray-400"
+            />
+            <textarea 
+               className="w-full rounded-md text-black px-2 py-2 border-2 border-gray-400 mt-2"
+               rows={10}
+            ></textarea>
+         </form>
       </section>
    )
 }
