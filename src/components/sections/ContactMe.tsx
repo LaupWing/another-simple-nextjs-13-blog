00237e9a2c9aee59ed2f9525112c966f7c9ed83b
LaupWing <loc_strict@hotmail.com>
2023-12-04 10:39:12 +0100

    udpate

---
+++ src/components/sections/ContactMe.tsx
@@ -0,0 +1,10 @@
+export const ContactMe = () => {
+   return (
+      <div className="flex flex-col">
+         <div className="flex flex-col">
+            <h2 className="text-sm uppercase text-gray-800">Contact me</h2>
+            <p className="text-gray-500">Subscribe to my newsletter to get regular content not published on my site.</p>
+         </div>
+      </div>
+   )
+}
