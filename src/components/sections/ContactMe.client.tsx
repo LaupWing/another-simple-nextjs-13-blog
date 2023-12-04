21b25e5cf16c1b9fe40295fe2f14aa907072ee15
LaupWing <loc_strict@hotmail.com>
2023-12-04 10:04:56 +0100

    created form ref

--- src/components/sections/ContactMe.client.tsx
+++ src/components/sections/ContactMe.client.tsx
@@ -1,7 +1,14 @@
+import { useRef } from "react"
 
 
 export const ContactMe = () => {
+   const form_ref = useRef<HTMLFormElement>(null)
+
    return (
-      <form>ContactMe.client</form>
+      <form
+         ref={form_ref}
+      >
+         
+      </form>
    )
 }
