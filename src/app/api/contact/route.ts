34913f69269d872aa0a2caf1508a27f0948122d6
LaupWing <loc_strict@hotmail.com>
2023-12-04 12:13:15 +0100

    creates transpoter

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,6 +3,16 @@ import nodemailer from "nodemailer"
 export async function POST(request: Request) {
    const data = await request.json()
    const { name, email, message } = data
+
+   const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+         user: "",
+         pass: ""
+      }
+   })
    
    return Response.json({ message: "ok" })
 }
