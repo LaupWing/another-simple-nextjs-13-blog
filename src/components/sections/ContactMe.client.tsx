92029944b08ac29dbd0a07ca7d674d33d9a3f80e
LaupWing <loc_strict@hotmail.com>
2023-12-04 10:17:58 +0100

    max w contianer

--- src/components/sections/ContactMe.client.tsx
+++ src/components/sections/ContactMe.client.tsx
@@ -10,7 +10,7 @@ export const ContactMe = () => {
          ref={form_ref}
          className="flex custom-container items-end gap-2"
       >
-         <div className="flex flex-col">
+         <div className="flex flex-col w-full max-w-[300px]">
             <label 
                htmlFor="email"
                className="uppercase tracking-wider text-sm font-bold text-gray-600 dark:text-gray-400"
@@ -18,7 +18,7 @@ export const ContactMe = () => {
                Email
             </label>
             <input 
-               className="py-2 px-2 rounded text-black bg-gray-100 border-2 border-gray-300"
+               className="py-2 px-2 w-full rounded text-black bg-gray-100 border-2 border-gray-300"
                type="email" 
                name="email" 
                id="email" 
