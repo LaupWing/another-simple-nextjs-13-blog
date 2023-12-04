ce6ea16575231d7b34ca1dfa6c9f29ef86c76425
LaupWing <loc_strict@hotmail.com>
2023-12-04 10:12:57 +0100

    input styling

--- src/components/sections/ContactMe.client.tsx
+++ src/components/sections/ContactMe.client.tsx
@@ -8,11 +8,16 @@ export const ContactMe = () => {
    return (
       <form
          ref={form_ref}
-         className="flex"
+         className="flex custom-container items-end gap-2"
       >
-         <div className="flex">
+         <div className="flex flex-col">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input 
+               className="py-2 px-2 rounded text-black bg-gray-100 border-2 border-gray-300"
+               type="email" 
+               name="email" 
+               id="email" 
+            />
          </div>
          <Button variant="gradient-animation">
             Submit
