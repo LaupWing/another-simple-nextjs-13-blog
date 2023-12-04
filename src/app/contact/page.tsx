2daa2e1e12c6fe89ff13ebaf772e909cd70ad710
LaupWing <loc_strict@hotmail.com>
2023-12-04 11:43:57 +0100

    addsubmit handler

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 import { Button } from "@/components/buttons/Button"
 import { Accent } from "@/components/elements/Accent"
-import { useRef } from "react"
+import { FormEvent, useRef } from "react"
 
 
 const Contact = () => {
    const form_ref = useRef<HTMLFormElement>(null)
+
+   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      console.log(e.currentTarget.elements.email)
+   }
 
    return (
       <section className="custom-container max-w-lg min-h-screen py-12">
@@ -19,13 +24,16 @@ const Contact = () => {
          <form 
             className="mt-4 flex w-full flex-col items-start gap-4"
             ref={form_ref}
+            onSubmit={handleSubmit}
          >
             <input 
                type="email" 
+               id="email"
                placeholder="Email"
                className="w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 focus:border-accent-dark focus:outline-none focus:ring-0 dark:focus:border-accent-light"
             />
             <textarea 
+               id="message"
                className="w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 focus:border-accent-dark focus:outline-none focus:ring-0 dark:focus:border-accent-light resize-none"
                placeholder="Message"
                rows={10}
