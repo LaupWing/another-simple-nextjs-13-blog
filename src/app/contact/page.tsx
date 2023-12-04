a67aa7904a9225720a9acf1f6402a45d539a65a2
LaupWing <loc_strict@hotmail.com>
2023-12-04 11:45:13 +0100

    added typings for this

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,36 +3,44 @@ import { Button } from "@/components/buttons/Button"
 import { Accent } from "@/components/elements/Accent"
 import { FormEvent, useRef } from "react"
 
+interface CustomElements extends HTMLFormControlsCollection {
+   name: HTMLInputElement
+   email: HTMLInputElement
+}
+
+interface CustomForm extends HTMLFormElement {
+   readonly elements: CustomElements;
+}
 
 const Contact = () => {
    const form_ref = useRef<HTMLFormElement>(null)
 
-   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+   const handleSubmit = async (e: FormEvent<CustomForm>) => {
       e.preventDefault()
       console.log(e.currentTarget.elements.email)
    }
 
    return (
       <section className="custom-container max-w-lg min-h-screen py-12">
-         <h1 
+         <h1
             className="text-3xl md:text-5xl"
             data-fade="0"
          >
             <Accent>Contact</Accent>
          </h1>
          <p className="text-gray-500">Unlock exciting possibilities! Interested in a project or tutoring? Reach out using the contact form and let's embark on this journey together!</p>
-         <form 
+         <form
             className="mt-4 flex w-full flex-col items-start gap-4"
             ref={form_ref}
             onSubmit={handleSubmit}
          >
-            <input 
-               type="email" 
+            <input
+               type="email"
                id="email"
                placeholder="Email"
                className="w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 focus:border-accent-dark focus:outline-none focus:ring-0 dark:focus:border-accent-light"
             />
-            <textarea 
+            <textarea
                id="message"
                className="w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 focus:border-accent-dark focus:outline-none focus:ring-0 dark:focus:border-accent-light resize-none"
                placeholder="Message"
