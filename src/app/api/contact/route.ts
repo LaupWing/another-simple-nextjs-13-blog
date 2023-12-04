3d4e848b334d569cf8764f565fecdce9e20e5b80
LaupWing <loc_strict@hotmail.com>
2023-12-04 11:57:40 +0100

    reafctore post

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,6 @@
-import { NextRequest, NextResponse } from "next/server"
-
-
-export async function POST(request: NextRequest, response: NextResponse) {
+export async function POST(request: Request) {
    console.log(request.body)
    const json = await request.json()
    console.log(json)
-   return NextResponse.json({ message: "ok" })
+   return Response.json({ message: "ok" })
 }
