72eb9ace98ee4004fd9b0b646c266d30f1398dee
laupwing <loc_strict@hotmail.com>
2023-12-07 09:07:13 +0100

    udpate

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,11 +22,12 @@ const About = () => {
                   Hello! I'm Loc Nguyen. Ever since I was young I have been interested in programming. I went to IT universities and tried it out, but unfortuanetly the education involved a lot of topics which not peak my interests, which is the coding part of it. Therefore made the choice to learn coding by myself. It started with watching a lot of videos on Youtube. Quickly I fell in love with web development and began to build websites on my own.
                </p>
                <p>
-                  I discovered that the coding part was kinda getting easy. So I started a new university revolving around web designt called communciation multimedia design. to learn more about the other parts of web development, such as design, SEO, and marketing. I also learned about the importance of soft skills, such as communication, time management, and teamwork. I believe that these skills are very important for a developer to have, and I am still learning and improving them every day.
+                  I discovered that the coding part was kinda getting easy. So I started a new university revolving around web design called communciation multimedia design to learn more about the other parts of web development.I also learned about the importance of soft skills, such as communication, time management, and teamwork. I believe that these skills are very important for a developer to have, and I am still learning and improving them every day.
                </p>
                <p>
-                  I have been working as a web developer for 5 years now. I have
+                  After finising my study I have been working as a web developer for 5 years now. I have
                   worked with many different technologies, but I am most
+                  
                   comfortable with React, Next.js, and Node.js. I am also
                   interested in learning more about DevOps and cloud
                   technologies.
