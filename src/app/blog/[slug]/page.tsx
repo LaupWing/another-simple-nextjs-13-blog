7d15f2f97c9b3de6431d2f38a9010c2a15f93562
LaupWing <loc_strict@hotmail.com>
2024-01-15 19:38:53 +0100

    udpate

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -29,7 +29,7 @@ export async function generateMetadata(props: PageProps): Promise<Metadata> {
    const post = await fetchPost(props.params.slug)
    const { frontmatter } = post
 
-   const OG_BANNER_LINK = `https://res.cloudinary.com/laupwing/image/upload/f_auto,c_fill,ar_4:5,w_1200/samples/sheep.jpg`
+   const OG_BANNER_LINK = `https://res.cloudinary.com/laupwing/image/upload/f_auto,c_fill,ar_4:5,w_1200/${frontmatter.banner}`
    
    return {
       ...seo({
