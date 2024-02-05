ebf37d05a436e57b1c6864049567e8a04ca6c389
laupwing <laupwing@gmail.com>
2024-02-05 12:19:00 +0100

    updated

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,68 +1,72 @@
 import type { ContentType, PickFrontmatter } from "@/types/frontmatters"
 
 interface OpenGraphType {
-   site_name: string
-   description: string
-   template_title?: string
-   logo?: string
-   banner?: string
-   is_blog?: boolean
+    site_name: string
+    description: string
+    template_title?: string
+    logo?: string
+    banner?: string
+    is_blog?: boolean
 }
 
 export const openGraph = ({
-   site_name,
-   template_title,
-   description,
-   banner,
-   logo = `${process.env.SITE_URL}/images/logo.png`,
-   is_blog = false
+    site_name,
+    template_title,
+    description,
+    banner,
+    logo = `${process.env.SITE_URL}/images/logo.png`,
+    is_blog = false,
 }: OpenGraphType): string => {
-   const og_logo = encodeURIComponent(logo)
-   const og_sitename = encodeURIComponent(site_name.trim())
-   const og_template_title = template_title
-      ? encodeURIComponent(template_title.trim())
-      : undefined
-   const og_description = encodeURIComponent(description.trim())
+    const og_logo = encodeURIComponent(logo)
+    const og_sitename = encodeURIComponent(site_name.trim())
+    const og_template_title = template_title
+        ? encodeURIComponent(template_title.trim())
+        : undefined
+    const og_description = encodeURIComponent(description.trim())
 
-   if (is_blog) {
-      const og_banner = banner
-         ? encodeURIComponent(banner.trim())
-         : undefined
+    if (is_blog) {
+        const og_banner = banner ? encodeURIComponent(banner.trim()) : undefined
 
-      return `${process.env.SITE_URL}/api/og/blog?templateTitle=${og_template_title}&banner=${og_banner}`
-   }
+        return `${process.env.SITE_URL}/api/og/blog?templateTitle=${og_template_title}&banner=${og_banner}`
+    }
 
-   return `${process.env.SITE_URL}/api/og/gradient?siteName=${og_sitename}&description=${og_description}&logo=${og_logo}${og_template_title ? `&templateTitle=${og_template_title}` : ""}`
+    return `${
+        process.env.SITE_URL
+    }/api/og/gradient?siteName=${og_sitename}&description=${og_description}&logo=${og_logo}${
+        og_template_title ? `&templateTitle=${og_template_title}` : ""
+    }`
 }
 
 export const getFromSessionStorage = (key: string) => {
-   if (typeof sessionStorage !== "undefined") {
-      return sessionStorage.getItem(key)
-   }
-   return null
+    if (typeof sessionStorage !== "undefined") {
+        return sessionStorage.getItem(key)
+    }
+    return null
 }
 
 export const attachContentMeta = async <T extends ContentType>(
-   frontmatter: Array<PickFrontmatter<T>>
+    frontmatter: Array<PickFrontmatter<T>>,
 ) => {
-   return await Promise.all(
-      frontmatter.map(async (frontmatter) => {
-         const res = await fetch(`${process.env.API_URL}/api/content/${frontmatter.slug}`)
-         const data = await res.json()
+    return await Promise.all(
+        frontmatter.map(async (frontmatter) => {
+            const res = await fetch(
+                `${process.env.API_URL}/api/content/${frontmatter.slug}`,
+            )
+            const data = await res.json()
 
-         return {
-            ...frontmatter,
-            views: data.content_views,
-            likes: data.content_likes
-         }
-      })
-   )
+            return {
+                ...frontmatter,
+                views: data.content_views,
+                likes: data.content_likes,
+            }
+        }),
+    )
 }
 
 export const fakeAwait = async (timeout = 1000) => {
-   await new Promise(resolve => {
-      setTimeout(() => {
-         resolve(true)
-      }, timeout)
-   })
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true)
+        }, timeout)
+    })
 }
