c63a07a88603ea4b8b55d775c4864e33972fd28f
laupwing <laupwing@gmail.com>
2024-02-06 18:02:37 +0100

    spaced

--- src/components/elements/Likes.client.tsx
+++ src/components/elements/Likes.client.tsx
@@ -6,35 +6,38 @@ import { useEffect, useState } from "react"
 import { LikeButtonLoading } from "../buttons/LikeButtonLoading"
 
 interface LikesProps {
-   slug: string
+    slug: string
 }
 
-export const Likes:FC<LikesProps> = ({ slug }) => {
-   const [data, setData] = useState({
-      all_likes: 0,
-      likes_by_user: 0
-   })
-   const [loading, setLoading] = useState(true)
+export const Likes: FC<LikesProps> = ({ slug }) => {
+    const [data, setData] = useState({
+        all_likes: 0,
+        likes_by_user: 0,
+    })
+    const [loading, setLoading] = useState(true)
 
-   const fetchLikes = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/like/${slug}`, {
-         method: "GET"
-      })
-      const data = await res.json()
-      setData(data)
-      setLoading(false)
-   }
+    const fetchLikes = async () => {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/api/like/${slug}`,
+            {
+                method: "GET",
+            },
+        )
+        const data = await res.json()
+        setData(data)
+        setLoading(false)
+    }
 
-   useEffect(() => {
-      fetchLikes()
-   }, [])
+    useEffect(() => {
+        fetchLikes()
+    }, [])
 
-   return loading ? (
-      <LikeButtonLoading />
-   ) : (
-      <LikeButton
-         all_likes={data.all_likes}
-         likes_by_user={data.likes_by_user}
-      />
-   )
+    return loading ? (
+        <LikeButtonLoading />
+    ) : (
+        <LikeButton
+            all_likes={data.all_likes}
+            likes_by_user={data.likes_by_user}
+        />
+    )
 }
