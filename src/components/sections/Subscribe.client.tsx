1a6ea567d9548e0a20e54ab1e6be2e0922ca70fe
laupwing <laupwing@gmail.com>
2024-02-06 18:16:22 +0100

    updated

--- src/components/sections/Subscribe.client.tsx
+++ src/components/sections/Subscribe.client.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { FormEvent, useRef, useState } from "react"
 import { Button } from "../buttons/Button"
+import { Rings } from "react-loader-spinner"
 
 export const Subscribe = () => {
     const form_ref = useRef<HTMLFormElement>(null)
@@ -10,36 +11,45 @@ export const Subscribe = () => {
     }
 
     return (
-        <form
-            ref={form_ref}
-            onSubmit={handleSubmit}
-            className="flex my-8 flex-col custom-container gap-4"
-        >
-            <div className="flex flex-col">
-                <h2 className="text-sm uppercase text-gray-800">Subscribe</h2>
-                <p className="text-gray-500">
-                    Subscribe to my newsletter to get regular content not
-                    published on my site.
-                </p>
-            </div>
-            <div className="flex flex-col items-start sm:flex-row sm:items-end gap-2">
-                <div className="flex gap-1 flex-col w-full max-w-[300px]">
-                    <label
-                        htmlFor="email"
-                        className="uppercase tracking-wider text-sm font-bold text-gray-600 dark:text-gray-400"
-                    >
-                        Email
-                    </label>
-                    <input
-                        className="py-2 px-2 w-full rounded text-black bg-gray-100 border-2 border-gray-300"
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Enter your email"
-                    />
+        <>
+            {loading && (
+                <div className="bg-black/20 fixed flex justify-center items-center inset-0 z-50">
+                    <Rings color="#b102b7" width={50} height={50} />
                 </div>
-                <Button variant="gradient-animation">Submit</Button>
-            </div>
-        </form>
+            )}
+            <form
+                ref={form_ref}
+                onSubmit={handleSubmit}
+                className="flex my-8 flex-col custom-container gap-4"
+            >
+                <div className="flex flex-col">
+                    <h2 className="text-sm uppercase text-gray-800">
+                        Subscribe
+                    </h2>
+                    <p className="text-gray-500">
+                        Subscribe to my newsletter to get regular content not
+                        published on my site.
+                    </p>
+                </div>
+                <div className="flex flex-col items-start sm:flex-row sm:items-end gap-2">
+                    <div className="flex gap-1 flex-col w-full max-w-[300px]">
+                        <label
+                            htmlFor="email"
+                            className="uppercase tracking-wider text-sm font-bold text-gray-600 dark:text-gray-400"
+                        >
+                            Email
+                        </label>
+                        <input
+                            className="py-2 px-2 w-full rounded text-black bg-gray-100 border-2 border-gray-300"
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Enter your email"
+                        />
+                    </div>
+                    <Button variant="gradient-animation">Submit</Button>
+                </div>
+            </form>
+        </>
     )
 }
