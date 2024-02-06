8a7f96f20350b8dc10e7b374dccb1b14153ed14c
laupwing <laupwing@gmail.com>
2024-02-06 17:32:45 +0100

    updated

--- src/components/elements/Views.client.tsx
+++ src/components/elements/Views.client.tsx
@@ -4,32 +4,36 @@ import { useEffect, useState } from "react"
 import { IconEye } from "../Icons"
 
 interface ViewsProps {
-   slug: string
+    slug: string
 }
 
-export const Views:FC<ViewsProps> = ({ slug }) => {
-   const [views, setViews] = useState<false|number>(false)
-   const fetchViews = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/view/${slug}`, {
-         method: "POST"
-      })
-      const data = await res.json()
-      setViews(data.content_views)
-   }
+export const Views: FC<ViewsProps> = ({ slug }) => {
+    const [views, setViews] = useState<false | number>(false)
+    const fetchViews = async () => {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/api/view/${slug}`,
+            {
+                method: "POST",
+            },
+        )
+        console.log(res)
+        const data = await res.json()
+        setViews(data.content_views)
+    }
 
-   useEffect(() => {
-      fetchViews()
-   }, [])
+    useEffect(() => {
+        fetchViews()
+    }, [])
 
-   return views ?  (
-      <div className="flex items-center gap-1">
-         <IconEye className="inline-block text-base" />
-         {views} views
-      </div>
-   ) : (
-      <div className="animate-pulse flex items-center gap-1">
-         <IconEye className="inline-block text-base" />
-         --- views
-      </div>
-   )
+    return views ? (
+        <div className="flex items-center gap-1">
+            <IconEye className="inline-block text-base" />
+            {views} views
+        </div>
+    ) : (
+        <div className="animate-pulse flex items-center gap-1">
+            <IconEye className="inline-block text-base" />
+            --- views
+        </div>
+    )
 }
