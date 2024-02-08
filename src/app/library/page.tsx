4f3ccfa3e45906ecfa3383f2c3ea57534f825d2e
laupwing <laupwing@gmail.com>
2024-02-08 16:27:49 +0100

    update

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -3,40 +3,37 @@ import { Accent } from "@/components/elements/Accent"
 import { getAllFilesFrontmatter } from "@/lib/mdx"
 import { Metadata } from "next"
 import seo from "@/lib/seo"
+import { attachContentMeta } from "@/lib/helpers"
 
 const fetchLibrary = async () => {
-   const library = await getAllFilesFrontmatter("library")
+    const library = await getAllFilesFrontmatter("library")
 
-   return library
+    return await attachContentMeta<"library">(library)
 }
 
 export const metadata: Metadata = {
-   ...seo({
-      as_path: "library",
-      title: "Library Page",
-      description: "Here's a collection of code snippets from various technologies that I have used before. Feel free to make use of them!"
-   })
+    ...seo({
+        as_path: "library",
+        title: "Library Page",
+        description:
+            "Here's a collection of code snippets from various technologies that I have used before. Feel free to make use of them!",
+    }),
 }
 
 const Library = async () => {
-   const posts = await fetchLibrary()
+    const posts = await fetchLibrary()
 
-   return (
-      <section className="custom-container py-12">
-         <h1 
-            className="text-3xl md:text-5xl"
-            data-fade="0"
-         >
-            <Accent>Library</Accent>
-         </h1>
-         <p 
-            className="mt-2 text-gray-600 dark:text-gray-300"
-            data-fade="1"
-         >Here's a collection of code snippets from various technologies that I have used before. Feel free to make use of them!</p>
-         <LibraryContainer 
-            posts={posts}
-         />
-      </section>
-   )
+    return (
+        <section className="custom-container py-12">
+            <h1 className="text-3xl md:text-5xl" data-fade="0">
+                <Accent>Library</Accent>
+            </h1>
+            <p className="mt-2 text-gray-600 dark:text-gray-300" data-fade="1">
+                Here's a collection of code snippets from various technologies
+                that I have used before. Feel free to make use of them!
+            </p>
+            <LibraryContainer posts={posts} />
+        </section>
+    )
 }
 export default Library
