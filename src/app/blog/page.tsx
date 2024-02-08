b8cb5a82827ef3a162cab29e163a23613db3fcd5
laupwing <laupwing@gmail.com>
2024-02-08 16:26:11 +0100

    upstead

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,42 +8,38 @@ import seo from "@/lib/seo"
 export const revalidate = 0
 
 const fetchBlogs = async () => {
-   const blogs = await getAllFilesFrontmatter("blog")
+    const blogs = await getAllFilesFrontmatter("blog")
 
-   return (await attachContentMeta<"blog">(blogs))
+    return await attachContentMeta<"blog">(blogs)
 }
 
 export const metadata: Metadata = {
-   ...seo({
-      as_path: "blog",
-      title: "Blog Page",
-      description: "Everything you related to web development regarding the technologies often used nowadays. Each week minimal 1 blog!"
-   })
+    ...seo({
+        as_path: "blog",
+        title: "Blog Page",
+        description:
+            "Everything you related to web development regarding the technologies often used nowadays. Each week minimal 1 blog!",
+    }),
 }
 
 const Blog = async () => {
-   const posts = await fetchBlogs()
-   
-   return (
-      <main>
-         <section className="custom-container py-12">
-            <h1 
-               className="text-3xl md:text-5xl"
-               data-fade="0"
-            >
-               <Accent>Blog</Accent>
-            </h1>
-            <p 
-               className="mt-2 text-gray-600 dark:text-gray-300"
-               data-fade="1"
-            >
-               Interesting tech findings.
-            </p>
-            <BlogsContainer 
-               posts={posts}
-            />
-         </section>
-      </main>
-   )
+    const posts = await fetchBlogs()
+
+    return (
+        <main>
+            <section className="custom-container py-12">
+                <h1 className="text-3xl md:text-5xl" data-fade="0">
+                    <Accent>Blog</Accent>
+                </h1>
+                <p
+                    className="mt-2 text-gray-600 dark:text-gray-300"
+                    data-fade="1"
+                >
+                    Interesting tech findings.
+                </p>
+                <BlogsContainer posts={posts} />
+            </section>
+        </main>
+    )
 }
 export default Blog
