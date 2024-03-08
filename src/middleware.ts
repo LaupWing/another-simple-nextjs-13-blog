34101ec3d8732e651d436cf1666dbb40e2c9b5b4
laupwing <loc_strict@hotmail.com>
2024-03-08 18:34:43 +0100

    Delete middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server"
 
-export function middleware(request: NextRequest) {
+export default function middleware(request: NextRequest) {
     // Check if there is any supported locale in the pathname
     const { pathname } = request.nextUrl
     // const pathnameHasLocale = locales.some(
