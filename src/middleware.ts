e60b692cf707ffd578b81195658856ed7281cc4a
laupwing <loc_strict@hotmail.com>
2024-04-05 19:19:22 +0200

    Update config matcher in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,5 +34,7 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    matcher: [
+        "/((?!api|_next/static|_next/image|favicon.ico|manifest.json).*)",
+    ],
 }
