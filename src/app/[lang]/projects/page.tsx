78ef77edcaac47d0c90333fab172908e5523cfea
laupwing <loc_strict@hotmail.com>
2024-04-05 18:54:04 +0200

    Refactor fetchProjects to accept language parameter

--- src/app/[lang]/projects/page.tsx
+++ src/app/[lang]/projects/page.tsx
@@ -1,41 +1,39 @@
 import { ProjectCard } from "@/components/cards/ProjectCard"
 import { Accent } from "@/components/elements/Accent"
+import { Locale } from "@/i18.config"
 import { getAllFilesFrontmatter } from "@/lib/mdx"
 
-const fetchProjects = async () => {
-   const projects = await getAllFilesFrontmatter("projects")
-   return projects
+const fetchProjects = async (lang: Locale) => {
+    const projects = await getAllFilesFrontmatter("projects", lang)
+    return projects
 }
 
-const Projects = async () => {
-   const projects = await fetchProjects()
+interface PageProps {
+    params: {
+        lang: Locale
+    }
+}
 
-   return (
-      <section className="custom-container py-12">
-         <h1 
-            className="text-3xl md:text-5xl"
-            data-fade="0"
-         >
-            <Accent>Projects</Accent>
-         </h1>
-         <p 
-            className="mt-2 text-gray-600 dark:text-gray-300"
-            data-fade="1"
-         >
-            My favorite projects.
-         </p>
-         <ul 
-            className="mt-6 grid gap-4 sm:grid-cols-2 xl:grid-cols-3"
-            data-fade="2"
-         >
-            {projects.map((project) => (
-               <ProjectCard 
-                  key={project.slug}
-                  project={project}
-               />
-            ))}
-         </ul>
-      </section>
-   )
+const Projects = async (pageProps: PageProps) => {
+    const projects = await fetchProjects(pageProps.params.lang)
+
+    return (
+        <section className="custom-container py-12">
+            <h1 className="text-3xl md:text-5xl" data-fade="0">
+                <Accent>Projects</Accent>
+            </h1>
+            <p className="mt-2 text-gray-600 dark:text-gray-300" data-fade="1">
+                My favorite projects.
+            </p>
+            <ul
+                className="mt-6 grid gap-4 sm:grid-cols-2 xl:grid-cols-3"
+                data-fade="2"
+            >
+                {projects.map((project) => (
+                    <ProjectCard key={project.slug} project={project} />
+                ))}
+            </ul>
+        </section>
+    )
 }
 export default Projects
