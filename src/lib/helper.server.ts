7b60fc4655c26e199261b149f65b312f4031948b
laupwing <loc_strict@hotmail.com>
2024-04-05 13:10:33 +0200

    Fix indentation and formatting in helper.server.ts

--- src/lib/helper.server.ts
+++ src/lib/helper.server.ts
@@ -3,40 +3,40 @@ import { z } from "zod"
 import { prisma_client } from "./prisma"
 
 export const getSessionId = (req: Request) => {
-   const ip_address = req.headers.get("x-forwarded-for") || "0.0.0.0"
+    const ip_address = req.headers.get("x-forwarded-for") || "0.0.0.0"
 
-   const current_user_id = createHash("md5")
-      .update(ip_address + (process.env.IP_ADDRESS_SALT as string), "utf8")
-      .digest("hex")
-   return current_user_id
-} 
+    const current_user_id = createHash("md5")
+        .update(ip_address + (process.env.IP_ADDRESS_SALT as string), "utf8")
+        .digest("hex")
+    return current_user_id
+}
 
 export const extractSlug = (req: Request) => {
-   console.log(req.url)
-   const splitted = req.url.split("/")
-   const available_endpoints = ["content", "like", "view"]
+    console.log(req.url)
+    const splitted = req.url.split("/")
+    const available_endpoints = ["content", "like", "view"]
 
-   if(available_endpoints.includes(splitted[splitted.length - 2])){
-      const slug = z.string().parse(splitted[splitted.length - 1])
-      return slug
-   }else {
-      throw new Error("Not a content API endpoint")
-   }
+    if (available_endpoints.includes(splitted[splitted.length - 2])) {
+        const slug = z.string().parse(splitted[splitted.length - 1])
+        return slug
+    } else {
+        throw new Error("Not a content API endpoint")
+    }
 }
 
 export const getUserLikeCount = async ({
-   session_id,
-   slug
+    session_id,
+    slug,
 }: {
-   session_id: string
-   slug: string
+    session_id: string
+    slug: string
 }) => {
-   return await prisma_client.like.count({
-      where: {
-         session_id: session_id,
-         ContentMeta: {
-            slug: slug
-         }
-      }
-   })
+    return await prisma_client.like.count({
+        where: {
+            session_id: session_id,
+            ContentMeta: {
+                slug: slug,
+            },
+        },
+    })
 }
