2774d8b664d918f7b6d7230aa47f52857b8ddf5e
laupwing <loc_strict@hotmail.com>
2024-04-06 14:29:50 +0200

    Refactor Content component and update tutorial description

--- src/components/sections/Content.client.tsx
+++ src/components/sections/Content.client.tsx
@@ -6,22 +6,20 @@ import { getMDXComponent } from "mdx-bundler/client"
 import { MDXComponents } from "@/components/MDXComponents"
 
 interface ContentProps {
-   code: string
+    code: string
 }
-export const Content:FC<ContentProps> = ({
-   code
-}) => {
-   const Component = useMemo(() => getMDXComponent(code), [code])
-   
-   return (
-      <article className="mdx lg:col-span-1 mt-4 projects prose mx-auto w-full transition-colors dark:prose-invert">
-         <Component
-            components={
-               {
-                  ...MDXComponents
-               } as any
-            }
-         />
-      </article>
-   )
+export const Content: FC<ContentProps> = ({ code }) => {
+    const Component = useMemo(() => getMDXComponent(code), [code])
+
+    return (
+        <article className="mdx lg:col-span-1 mt-4 projects prose mx-auto w-full transition-colors dark:prose-invert">
+            <Component
+                components={
+                    {
+                        ...MDXComponents,
+                    } as any
+                }
+            />
+        </article>
+    )
 }
